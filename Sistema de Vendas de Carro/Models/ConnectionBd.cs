9d18a68a43a8d37e06c050c871aee8ebdd4553a8
Yugi <ka-ladze@hotmail.com>
2016-06-29 01:20:38 -0300

    fazendo a ligaçao com o bando de dados

---
+++ Sistema de Vendas de Carro/Models/ConnectionBd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+
+namespace Sistema_de_Vendas_de_Carro.Models
+{
+    public class ConnectionBd
+    {
+        public ConnectionBd()
+        {
+            connectionBd.Open();
+        }
+
+        public NpgsqlConnection connectionBd = new NpgsqlConnection("User ID=postgres;Password=99516118;Host=localhost;Port=5432;Database=VendaCarros;");
+
+    }
+}
